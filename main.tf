a1febb109427ecce87090c2ec38b502cd1326e72
Maumita673 <maumitadey29@gmail.com>
2025-01-22 11:44:52 +0530

    Update main.tf
    # Change this to a unique name

--- main.tf
+++ main.tf
@@ -2,11 +2,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name"
-}
-
 resource "aws_instance" "my_instance" {
   ami           = "ami-0df8c184d5f6ae949"
   instance_type = "t2.micro"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket-name-12345" # Change this to a unique name
+  acl    = "private"
 }
