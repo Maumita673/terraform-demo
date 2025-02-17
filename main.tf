74124e0f3961ccd0c2363d3bb5de37612cb2846d
Maumita673 <maumitadey29@gmail.com>
2025-02-17 20:23:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,6 @@ module "vpc" {
   cidr_block        = var.vpc_cidr_block
   subnet_cidr_block = var.subnet_cidr_block
   availability_zone = var.availability_zone
-  vpc_name          = var.vpc_name
-  subnet_name       = var.subnet_name
 }
 
 module "ec2" {
@@ -13,6 +11,5 @@ module "ec2" {
   ami           = var.ami
   instance_type = var.instance_type
   subnet_id     = module.vpc.subnet_id
-  instance_name = var.instance_name
 }
 
