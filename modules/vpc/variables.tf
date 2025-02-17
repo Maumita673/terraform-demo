6c9b7c7a96f93e1fccebaba582267da79cfb5738
Maumita673 <maumitadey29@gmail.com>
2025-02-17 19:39:34 +0530

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,1 +1,15 @@
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "subnet_cidr_block" {
+  description = "CIDR block for the Subnet"
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "Availability Zone"
+  type        = string
+}
 
