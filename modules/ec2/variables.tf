e2333ba1036c4ee602564e259353ac973f206486
Maumita673 <maumitadey29@gmail.com>
2025-02-17 19:37:40 +0530

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,1 +1,15 @@
+variable "ami" {
+  description = "AMI ID"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance Type"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID"
+  type        = string
+}
 
