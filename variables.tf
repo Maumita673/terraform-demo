d008ceb14b44fba15eeefc29f9d4fd66fbd4109b
Maumita673 <maumitadey29@gmail.com>
2025-02-17 19:43:57 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,30 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources."
+  type        = string
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "subnet_cidr_block" {
+  description = "CIDR block for the Subnet"
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "Availability Zone"
+  type        = string
+}
+
+variable "ami" {
+  description = "AMI ID"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance Type"
+  type        = string
+}
 
